08c53a28a957c2725a51084fc0b38f461777e36b
Black <82063930+Blackkk0@users.noreply.github.com>
2021-06-29 22:21:39 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -2,12 +2,28 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "help",
-  aliases: ["h"],
   description: "Help Command!",
   usage: "o/Help | <Command Name>",
   run: async(client, message, args) => {
     
-  let embed = new MessageEmbed
+  let embed = new Discord.MessageEmbed()
+  .setDescription(`
+**General Commands**
+\`userinfo\`,\`serverinfo\`,\`botinfo\`,
+\`weather\`,\`avatar\`,\`serveravatar\`,
+\`invite\`,\`roles\`,\`listemoji\`,
+\`allbots\`,\`roleinfo\`
+**Moderation Commands**
+\`backup\`,\`mute\`,\`unmute\`,
+\`ban\`,\`unban\`,\`kick\`,
+\`clear\`,\`lock\`,\`unlock\`,
+\`channelhide\`,\`channelshow\`,
+\`vkick\`,\`vmute\`,\`vunmute\`,
+\`deafen\`,\`undeafen\`,\`moveall\`,
+\`slowmode\`
+
+[Invite]() , [Support]()
+`)
     
   message.channel.send(embed);
   
