536fa05fed3c5478e073e8ff86f162efaccfd44a
Black <82063930+Blackkk0@users.noreply.github.com>
2021-06-29 22:32:55 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "help",
+  aliases:["commands"],
   description: "Help Command!",
   usage: "o/Help | <Command Name>",
   run: async(client, message, args) => {
