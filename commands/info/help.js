2c18d9bb96fb93a2c8debcb79b08d93f6ce2ee01
Black <82063930+Blackkk0@users.noreply.github.com>
2021-06-30 02:39:35 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 module.exports = {
   name: "help",
   aliases:[],
-  description: "Help Command!",
-  usage: "o/Help | <Command Name>",
+  description: "",
+  usage: "",
   run: async(client, message, args) => {
     
   let embed = new Discord.MessageEmbed()
