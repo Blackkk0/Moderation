dc80735cf68a6c2712038ae7f18edccac7cbef2f
Black <82063930+Blackkk0@users.noreply.github.com>
2021-06-30 02:41:24 +0300

    Update serveravatar.js

--- commands/info/serveravatar.js
+++ commands/info/serveravatar.js
@@ -3,17 +3,17 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "serveravatar",
-  aliases: ["sv"],
+  aliases: [],
   description: "Foto Profil / Avatar!",
   usage: "Avatar | <Mention Member>",
   run: async (client, message, args) => {
-    //Start 
+    
     
     let embed = new Discord.MessageEmbed()
     
       embed.setDescription(`[Download](${message.guild.iconURL({ dynamic: true, size: 1024 })})`)
       embed.setImage(message.guild.iconURL({ dynamic: true, size: 1024 }))
-      embed.setColor("BLACK")
+      embed.setColor("")
     
       message.channel.send(embed)
     
